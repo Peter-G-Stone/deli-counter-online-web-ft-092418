8d83950a71ca45f402dd027c656ea8c9b88ae7b0
Peter Stone <peter.stone.online@gmail.com>
2018-09-26 15:41:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+def line(deliLine)
+  statement = "The line is currently"
+  i = 1
+  
+  if deliLine.size == 0 
+    statement = statement + " empty."
+  else
+    statement = statement + ":"
+    deliLine.each do |pers|
+      statement = statement + " #{i}. #{pers}"
+      i += 1
+    end
+  end
+
+  puts statement
+end 
+
+
+
+def take_a_number(deliLine, newPers)
+  deliLine.push(newPers)
+  statement = "Welcome, #{newPers}. You are number #{deliLine.size} in line."
+  puts statement
+end 
+
+
+
+def now_serving(deliLine)
+  if deliLine.size > 0 
+    puts "Currently serving #{deliLine.first}."
+    deliLine.shift
+  else 
+    puts "There is nobody waiting to be served!"
+  end 
+end 
+
+
+
