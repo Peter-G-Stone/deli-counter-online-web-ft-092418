46cec1677efd574d915de7ba112f0c75dd7528d7
Peter Stone <peter.stone.online@gmail.com>
2018-09-26 15:33:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,20 @@
 def line(deliLine)
+  
   statement = "The line is currently"
+  i = 1
+  
   if deliLine.size == 0 
-    statement.push(" empty.")
-end 
+    statement = statement + " empty."
+  else
+    statement = statement + ":"
+    deliLine.each do |pers|
+      statement = statement + " #{i}. #{pers}"
+      i += 1
+    end
+  end
 
+  puts statement
+end 
 
 
 def take_a_number(deliLine, newPers)
